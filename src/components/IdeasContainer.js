6ffce6515bd62da15b1042dbca92f13aa4202f22
Nguyen Thi Huyen Lanh <nguyen.thi.huyen.lanh@sun-asterisk.com>
2019-11-15 09:14:53 +0700

    hot fix: reverse between api port and frontend port

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -10,7 +10,7 @@ class IdeasContainer extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3001/api/v1/ideas.json')
+    axios.get('http://localhost:3000/api/v1/ideas.json')
     .then(response => {
       console.log(response)
       this.setState({ideas: response.data})
@@ -21,7 +21,14 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return(
+            <div className="tile" key={idea.id} >
+              <h4>{idea.title}</h4>
+              <p>{idea.body}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }

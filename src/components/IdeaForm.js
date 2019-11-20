ef8e09e5715752cd8611354924a3403d01b8e12b
Nguyen Thi Huyen Lanh <nguyen.thi.huyen.lanh@sun-asterisk.com>
2019-11-20 15:16:37 +0700

    Editing an existing idea

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -36,7 +36,7 @@ class IdeaFrom extends Component {
     return (
       <div className="title">
         <form onBlur={this.handleBlur} >
-          <input className="input" type="text" name="title" placeholder="Enter a title pleaseeee" value={this.state.title} onChange={this.handleInput} />
+          <input className="input" type="text" name="title" placeholder="Enter a title pleaseeee" value={this.state.title} onChange={this.handleInput} ref={this.props.titleRef}/>
           <textarea className="input" name="body" placeholder="Describe your idea" value={this.state.body} onChange={this.handleInput}></textarea>
         </form>
       </div>
